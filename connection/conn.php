520979ff12bf659b13d027706fc0325859b8c0a9
Intuneteq <tobiolanitori@gmail.com>
2023-11-13 17:55:05 +0000

    feat: running db conn via env

--- connection/conn.php
+++ connection/conn.php
@@ -1,7 +1,20 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$db_host = $_ENV['DB_HOST'];
+$db_username = $_ENV['DB_USERNAME'];
+$db_password = $_ENV['DB_PASSWORD'];
+$db_database = $_ENV['DB_DATABASE'];
+
 try {
-    $conn = new mysqli('localhost', 'root', '', 'box');
+    $conn = new mysqli($db_host, $db_username, $db_password, $db_database);
+
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
--- core/initialize.php
+++ core/initialize.php
@@ -1,6 +1,6 @@
 <?php
 defined('DS') ? null : define("DS", DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define("SITE_ROOT", $_SERVER['DOCUMENT_ROOT'] . DS . 'boxOfHope');
+defined('SITE_ROOT') ? null : define("SITE_ROOT", $_SERVER['DOCUMENT_ROOT'] . DS . 'box-of-hope');
 
 defined("INC_PATH") ? null : define("INC_PATH", SITE_ROOT . DS . 'connection');
 defined("CORE_PATH") ? null : define("CORE_PATH", SITE_ROOT . DS . 'core');
